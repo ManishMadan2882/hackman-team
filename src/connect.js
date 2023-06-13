d3d46a0d08acf0516a01957f0cbb60c86c27438d
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-06-13 23:37:12 +0530

    Add files via upload

--- src/connect.js
+++ src/connect.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-console.log(process.env.MONGOURL);
 module.exports = mongoose.connect(process.env.MONGOURL)
 .then( () => {
     console.log("Connection with database : success");
