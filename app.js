8ec0e894dcda003f9cf204af0861a418fc37c511
Aditya Khattri <adityakhattri123@gmail.com>
2023-06-18 17:29:09 +0530

    Revert Commit follows

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 const lawyer = require("./routes/lawyerRoute");
 const user = require("./routes/userRoute");
+const post = require("./routes/postRoute")
 
 app.use("/api/v1",lawyer);
 app.use("/api/v1",user);
+app.use("/api/v1",post);
 
 module.exports = app;
 
