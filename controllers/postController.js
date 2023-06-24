d2b5a8711b0d86eb9e9e05e9a08c0ab2a14920c1
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-06-24 12:37:27 +0530

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -85,7 +85,7 @@ exports.addComment = async(req,res,next)=>{
 
      res.status(200).json({
         success:true,
-        message:"Comment Added Successfully"
+        message:"Comment Added Successfully!"
      })
     }
     else{
